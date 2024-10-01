81138bc561010980fbf2abdf047e2cff87df2fc6
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2024-10-01 08:55:34 +0300

    Ongoing

--- server/app.py
+++ server/app.py
@@ -4,5 +4,35 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index() :
+    return '<h1>Python Operations with Flask Routing and Views<h1>'
+
+@app.route('/print/<parameter>')
+def print_string(parameter) :
+    print (parameter)
+    pass
+
+@app.route('/count/<number>')
+def count( number ) :
+    for i in range ( number ) :
+        print ( i, end="\n" )
+    pass
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math( num1, operation, num2 ) :
+    if (operation == '+') :
+        return num1 + num2
+    elif (operation == '-') :
+        return num1 - num2
+    elif (operation == '*') :
+        return num1 * num2
+    elif (operation == 'div') :
+        return num1 / num2
+    elif (operation == '%') :
+        return num1 % num2
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
